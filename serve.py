e7a0a42fcf30a6e4a2efe273c0f3b5aaf31580c1
Alvaro Perez-Diaz <alvpdiaz@gmail.com>
2018-01-29 11:16:09 +0000

    Updated the twitter library to Twython

--- serve.py
+++ serve.py
@@ -1,6 +1,7 @@
 import glob
 import os
-import tweepy
+from twython import TwythonStreamer
+from twython import Twython
 import requests
 import imageio
 import cv2
@@ -14,10 +15,8 @@ with open("credentials.txt") as f:
     access_token = f.readline().strip()
     access_token_secret = f.readline().strip()
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
-
-api = tweepy.API(auth)
+twitter = Twython(consumer_key, consumer_secret,
+                  access_token, access_token_secret)
 
 # Generate GIF. Based on:
 # http://www.idiotinside.com/2017/06/06/create-gif-animation-with-python/
@@ -30,52 +29,50 @@ def create_gif(filenames, duration):
     imageio.mimsave(output_file, images, duration=duration)
 
 # Stream
-class MyStreamListener(tweepy.StreamListener):
-    def on_status(self, status):
-        id_ = status.id
-        text_ = status.text
-        sender_ = status.user.screen_name
+class MyStreamListener(TwythonStreamer):
+    def on_success(self, status):
+        id_str = status['id_str']
+        text_ = status['text']
+        sender_ = status['user']['screen_name']
         print("============================================")
         print("Recieved tweet from {}".format(sender_))
 
-        try:
-            media_url = status.entities['media'][0]['media_url_https']
-            print("Image URL is {}".format(media_url))
+        # try:
+        media_url = status['entities']['media'][0]['media_url_https']
+        print("Image URL is {}".format(media_url))
 
-            # Download and save image
-            path = "server/in/"
-            filename = str(id_) + '.jpeg'
-            f = open(path + filename, 'wb')
-            f.write(requests.get(media_url).content)
-            f.close()
+        # Download and save image
+        path = "server/in/"
+        filename = str(id_str) + '.jpeg'
+        f = open(path + filename, 'wb')
+        f.write(requests.get(media_url).content)
+        f.close()
 
-            # Call the simulation on this downloaded file
-            # This should result on several images placed in /server/output
-            print("Started simulation")
-            run_sim(path + filename)
+        # Call the simulation on this downloaded file
+        # This should result on several images placed in /server/output
+        print("Started simulation")
+        run_sim(path + filename)
 
-            # Generate GIF
-            print("Creating GIF")
-            create_gif(sorted(glob.glob('server/out/{}*.png'.format(id_))), 0.15)
+        # Generate GIF
+        print("Creating GIF")
+        create_gif(sorted(glob.glob('server/out/{}*.png'.format(id_str))), 0.15)
 
-            # Tweet back with the image!
-            print("Tweeting back")
-            filename = "server/out/{}.gif".format(str(id_))
-            message = "Here is the simulation in your image! @{}".format(sender_)
-            api.update_with_media(filename, status=message, in_reply_to_status_id=id_)
+        # Tweet back with the image!
+        print("Tweeting back")
+        photo = open('server/out/{}.gif'.format(str(id_str)), 'rb')
+        response = twitter.upload_media(media=photo)
+        message = "Here is the simulation in your image! @{}".format(sender_)
+        twitter.update_status(status=message, media_ids=[response['media_id']],
+                              in_reply_to_status_id=id_str)
+        print("All done successfully!")
 
-            # Trying to clean everything
-            # print("Attempting to clean things up")
-            # cv2.destroyAllWindows()
-            # cv2.waitKey(1)
-            # cv2.waitKey(1)
-            # cv2.waitKey(1)
-        except:
-            pass
+        # except:
+        #     pass
 
         print()
 
 if __name__ == "__main__":
-    myStreamListener = MyStreamListener()
-    myStream = tweepy.Stream(auth = api.auth, listener=myStreamListener)
-    myStream.filter(track=['@FluidFlowTest'], async=True)
+    myStream = MyStreamListener(consumer_key, consumer_secret,
+                              access_token, access_token_secret)
+
+    myStream.statuses.filter(track='@FluidFlowTest')
